589f14329567529968a54879d0e19bfe7902a5dd
Mike Nsubuga <nsubugamike021@gmail.com>
2021-08-15 19:33:37 +0300

    load members component async

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ViewContainerRef } from '@angular/core';
+import { ComponentFactoryResolver } from '@angular/core';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -7,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
+  @ViewChild('membersmaptemp', {read: ViewContainerRef})
+  private membersviewcontainerref: ViewContainerRef;
 
-  constructor() { }
+  constructor(private vcref: ViewContainerRef, private cfr: ComponentFactoryResolver) { }
 
   ngOnInit(): void {
   }
 
- 
-
+ async loadMembersComponent(){
+   this.vcref.clear();
+   const {MembersMapComponent} = await import('./members-map/members-map.component')
+  // let membersmapcomp = this.vcref.createComponent(this.cfr.resolveComponentFactory(MembersMapComponent));
+  this.membersviewcontainerref.createComponent(this.cfr.resolveComponentFactory(MembersMapComponent));
+  }
+  
 }
